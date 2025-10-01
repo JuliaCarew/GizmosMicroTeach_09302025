01b63a5d0e822de2f513095359cc90b624cfda65
JuliaCarew <crewjulia3@gmail.com>
2025-10-01 14:28:02 -0300

    g

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    
+    public Transform endPoint;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, endPoint.position);
+    }
+
+
 }
